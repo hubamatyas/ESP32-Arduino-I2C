121b15c33cc1cb21f792fc67d8529527308e4ca4
hubamatyas <hubamatyas@gmail.com>
2022-09-06 11:43:08 +0100

    Create esp32_thingsboard.ino

---
+++ esp32_thingsboard.ino
@@ -0,0 +1,261 @@
+#include "ThingsBoard.h"
+#include <WiFi.h>
+#include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
+#include <Wire.h>
+
+// define wife ssid, identity and password
+#define EAP_IDENTITY "zcabhoj@ucl.ac.uk"
+#define EAP_PASSWORD "yourpassword"
+#define ssid "eduroam"
+
+// define token and server
+#define TOKEN               "DssDXSgSIhf7UchV8WO2"//"0ytHNB5jCAyQYxkAlBfs"
+#define THINGSBOARD_SERVER  "demo.thingsboard.io"
+
+// Baud rate for debug serial
+#define SERIAL_DEBUG_BAUD   115200
+
+// define pins on esp32
+#define SLAVE_ADDR 9
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+bool heater_power = true;
+bool motor_power = true;
+bool pump_power = true;
+
+float tar_temp = 0.0;
+int tar_rpm = 0;
+float tar_ph = 0.0;
+
+float temp;
+float ph;
+int rpm;
+
+WiFiClient espClient;           // Initialize ThingsBoard client
+PubSubClient client(espClient); //Initailise client instance
+ThingsBoard tb(espClient);      // Initialize ThingsBoard instance
+int status = WL_IDLE_STATUS;    // the Wifi radio's status
+
+
+void InitWiFi()
+{
+  Serial.println();
+  Serial.print("Connecting to ");
+  // attempt to connect to WiFi network
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+  esp_wifi_sta_wpa2_ent_enable();
+  
+  WiFi.begin(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println("");
+  Serial.println(F("Connected to AP successfully!"));
+  Serial.print(F("IP address set: "));
+  Serial.println(WiFi.localIP()); // print LAN IP
+}
+
+void reconnectWiFi() {
+  // Loop until we're reconnected
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+  esp_wifi_sta_wpa2_ent_enable();
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(ssid);
+    Serial.print("reconnecting");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+  }
+}
+
+// Processes function for RPC call "example_set_temperature"
+// RPC_Data is a JSON variant, that can be queried using operator[]
+// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+RPC_Response processTemperatureChange(const RPC_Data &data)
+{
+  Serial.println("Received the set temperature RPC method");
+
+  // Process data
+  float temperature = data;
+  tar_temp = temperature;
+  Serial.print("Example temperature: ");
+  Serial.println(temperature);
+
+  // Just an response example
+  return RPC_Response();
+}
+
+RPC_Response processPhChange(const RPC_Data &data)
+{
+  Serial.println("Received the set ph RPC method");
+
+  // Process data
+  float Ph = data;
+  tar_ph = Ph;
+  Serial.print("Ph value: ");
+  Serial.println(Ph);
+
+  // Just an response example
+  return RPC_Response();
+}
+
+RPC_Response processRpmChange(const RPC_Data &data)
+{
+  Serial.println("Received the set rpm RPC method");
+
+  // Process data
+  int rpm = data;
+  tar_rpm = rpm;
+  Serial.print("rpm : ");
+  Serial.println(rpm);
+
+  // Just an response example
+  return RPC_Response();
+}
+
+// Processes function for RPC call "example_set_switch"
+// RPC_Data is a JSON variant, that can be queried using operator[]
+// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+RPC_Response processSwitchChange(const RPC_Data &data)
+{
+  Serial.println("Received the set switch method");
+
+  // Process data
+
+  bool switch_state = data;
+  
+  Serial.print("Example switch state: ");
+  Serial.println(switch_state);
+
+  // Just an response example
+  return RPC_Response();
+}
+
+const size_t callbacks_size = 4;
+RPC_Callback callbacks[callbacks_size] = {
+  { "setTemperature",    processTemperatureChange },
+  { "setSwitch",         processSwitchChange },
+  { "setPh",             processPhChange },
+  { "setRpm",            processRpmChange }
+};
+
+bool subscribed = false;
+
+// encodes integers to fit 1 byte Wire.write()
+void encode(double num, int* ptr) {
+  *ptr = int(num/256);
+  *(ptr+1) = int(num)%256;
+}
+
+void setup() {
+  // configure the pins
+  Wire.begin (I2C_SDA, I2C_SCL);
+  // initialize serial for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  InitWiFi();
+  //client.setServer(THINGSBOARD_SERVER, 1883);
+  //client.setCallback(callback);
+}
+
+void loop() {
+  delay(1500);
+
+  // request 6 bytes from slave
+  Wire.requestFrom(SLAVE_ADDR, 6);
+  while (Wire.available()) {
+    temp = int(Wire.read()) * 256;
+    temp += int(Wire.read());
+    rpm = int(Wire.read()) * 256;
+    rpm += int(Wire.read());
+    ph = int(Wire.read()) * 256;
+    ph += int(Wire.read());
+  }
+  // debugging print statements
+  temp = temp / 100;
+  ph = ph / 100;
+  Serial.println();
+  Serial.println("data from Arduino: ");
+  Serial.println(temp);
+  Serial.println(rpm);
+  Serial.println(ph);
+  Serial.println(); // print the character
+
+
+
+  // reconnect to WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnectWiFi();
+  }
+  client.loop();
+
+  // reconnect to ThingsBoard
+  if (!tb.connected()) {
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
+
+  Serial.println("Sending data...");
+
+  // Uploads new telemetry to ThingsBoard using MQTT.
+  // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api
+  // for more details
+  
+  
+  tb.sendTelemetryFloat("temperature", temp);
+  tb.sendTelemetryFloat("ph", ph);
+  tb.sendTelemetryInt("rpm", rpm);
+  
+  if (!subscribed) {
+    Serial.println("Subscribing for RPC...");
+
+    // Perform a subscription. All consequent data processing will happen in
+    // processTemperatureChange() and processSwitchChange() functions,
+    // as denoted by callbacks[] array.
+    if (!tb.RPC_Subscribe(callbacks, callbacks_size)) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }
+
+    Serial.println("Subscribe done");
+    subscribed = true;
+  }
+
+  Serial.println("Waiting for data...");
+
+    // send data to slave
+  Wire.beginTransmission(SLAVE_ADDR); // transmit to device
+  int* temp_encoded;
+  int* rpm_encoded;
+  int* ph_encoded;
+  encode(tar_temp*100, temp_encoded);
+  encode(tar_rpm, rpm_encoded);
+  encode(tar_ph*100, ph_encoded);
+  
+  Wire.write(*temp_encoded);
+  Wire.write(*(temp_encoded+1));
+  Wire.write(*rpm_encoded);
+  Wire.write(*(rpm_encoded+1));
+  Wire.write(*ph_encoded);
+  Wire.write(*(ph_encoded+1));
+  Wire.endTransmission();  
+  
+  tb.loop();
+}
